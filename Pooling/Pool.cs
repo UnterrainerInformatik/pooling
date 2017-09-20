3d66e0553e76c5388365131c581b43160e60b89e
guFalcon <gu_falcon@hotmail.com>
2017-09-20 22:50:02 +0200

    Adds icon. Corrects license.

--- Pooling/Pool.cs
+++ Pooling/Pool.cs
@@ -1,15 +1,28 @@
 ﻿// *************************************************************************** 
-//  Copyright (c) 2015 by Unterrainer Informatik OG.
-//  This source is licensed to Unterrainer Informatik OG.
-//  All rights reserved.
-//  
-//  In other words:
-//  YOU MUST NOT COPY, USE, CHANGE OR REDISTRIBUTE ANY ART, MUSIC, CODE OR
-//  OTHER DATA, CONTAINED WITHIN THESE DIRECTORIES WITHOUT THE EXPRESS
-//  PERMISSION OF Unterrainer Informatik OG.
-// ---------------------------------------------------------------------------
-//  Programmer: G U, 
-//  Created: 2015-09-09
+// This is free and unencumbered software released into the public domain.
+// 
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+// 
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// 
+// For more information, please refer to <http://unlicense.org>
 // ***************************************************************************
 
 using System;
@@ -43,7 +56,7 @@ namespace Pooling
 
         public Pool()
         {
-            constructorParameters = new object[] { };
+            constructorParameters = new object[] {};
         }
 
         /// <summary>
@@ -65,7 +78,7 @@ namespace Pooling
             if (!pool.TryDequeue(out result))
             {
                 // The queue was empty. Construct new T.
-                result = (T)Activator.CreateInstance(typeof(T), constructorParameters);
+                result = (T) Activator.CreateInstance(typeof(T), constructorParameters);
                 Created?.Invoke(this, new PoolEventArgs<T>(result));
                 CreationCount++;
             }
@@ -105,7 +118,8 @@ namespace Pooling
         {
             T item;
             while (pool.TryDequeue(out item))
-            {}
+            {
+            }
             return this;
         }
 
